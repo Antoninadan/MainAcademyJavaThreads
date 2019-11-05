67cf9cade48b25a3b9181ce33d87d28422c3c6a3
Antoninadan <mail100@i.ua>
2019-11-05 23:04:37 +0200

    add LOMBOK

--- src/com/mainacad/ApplicationRunner.java
+++ src/com/mainacad/ApplicationRunner.java
@@ -11,45 +11,74 @@ public class ApplicationRunner {
     private static final Logger LOG = Logger.getLogger(ApplicationRunner.class.getName());
 
     public static void main(String[] args) {
-        List<String> connectionIPList = Collections.synchronizedList(new ArrayList<>());
-        List<Thread> threads = new ArrayList<>();
-
-        ConnectionInfo connectionInfo = ConnectionHelperInfo.getRandomConnactionInfo();
-        for (int i = 1; i <= 100; i++) {
-            MultithreadWrapper multithreadWrapper = new MultithreadWrapper("thread " + i, connectionInfo, connectionIPList);
-            threads.add(multithreadWrapper);
-            multithreadWrapper.start();
-        }
-        while (threadsAlive(threads)) {
-            try {
-                LOG.info("\n__________________________________________\nThreads still alive");
-                Thread.sleep(2000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-        LOG.info(connectionIPList.size() + " connection were written to file");
-
-        // MAP ///////////////////////////////////////////////////////////////////////////////////////////
-
-        Map<String, String> users = new HashMap<>();
-        users.put("111", "Alex1");
-        users.put("118", "Alex8");
-        users.put("113", "Alex3");
-        users.put("114", "Alex4");
-        users.put("115", "Alex5");
-
-        users.put("111", "Nikita");
-        users.put(null, "Gleb");
-
-        System.out.println(users.toString());
-
-//        for (String key : users.keySet()) {
-//            String value = users.get(key);
-//            LOG.info("Key with " + key + " has value \"" + value + "\"");
+//        List<String> connectionIPList = Collections.synchronizedList(new ArrayList<>());
+//        List<Thread> threads = new ArrayList<>();
+//
+//        ConnectionInfo connectionInfo = ConnectionHelperInfo.getRandomConnactionInfo();
+//        for (int i = 1; i <= 100; i++) {
+//            MultithreadWrapper multithreadWrapper = new MultithreadWrapper("thread " + i, connectionInfo, connectionIPList);
+//            threads.add(multithreadWrapper);
+//            multithreadWrapper.start();
 //        }
+//
+//        while (threadsAlive(threads)) {
+//            try {
+//                LOG.info("\n__________________________________________\nThreads still alive");
+//                Thread.sleep(2000);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//        LOG.info(connectionIPList.size() + " connection were written to file");
 
-        users.keySet().forEach(key -> LOG.info("Key with " + key + " has value \"" + users.get(key) + "\""));
+//        // MAP ///////////////////////////////////////////////////////////////////////////////////////////
+//
+//        Map<String, String> users = new HashMap<>();
+//        users.put("111", "Alex1");
+//        users.put("118", "Alex8");
+//        users.put("113", "Alex3");
+//        users.put("114", "Alex4");
+//        users.put("115", "Alex5");
+//
+//        users.put("111", "Nikita");
+//        users.put(null, "Gleb");
+//
+//        System.out.println(users.toString());
+//
+////        for (String key : users.keySet()) {
+////            String value = users.get(key);
+////            LOG.info("Key with " + key + " has value \"" + value + "\"");
+////        }
+//
+//        users.keySet().forEach(key -> LOG.info("Key with " + key + " has value \"" + users.get(key) + "\""));
+
+//        Operation operation = new Operation() {
+//            @Override
+//            public int calculate(int num1, int num2) {
+//                return num1 + num2;
+//            }
+//        };
+
+//        // functional interface
+//        Operation operation = ((num1, num2) -> num1 + num2);
+//        int result = operation.calculate(10, 20);
+//        System.out.println("result = " + result);
+//
+//        operation = ((num1, num2) -> num1 + num2 + 1000);
+//        result = operation.calculate(10, 20);
+//        System.out.println("result = " + result);
+//
+//
+//        // streams
+//        List<Integer> numbers = new ArrayList<>();
+//        for (int i = 1; i < 20; i++) {
+//            numbers.add(i);
+//        }
+//
+//        numbers.stream().filter(it -> it % 2 != 0).forEach(it -> LOG.info("Filter is " + it));
+//
+//        System.out.println("numbers = " + numbers);
+//        System.out.println("numbers = " + numbers);
     }
 
     private static boolean threadsAlive(List<Thread> threads) {
@@ -60,4 +89,19 @@ public class ApplicationRunner {
         }
         return false;
     }
+
+    class InnerClas {  // static
+
+    }
+
+    enum InnerEnum {
+        ONE,
+        TWO
+    }
+
+    interface Operation {
+        int calculate(int num1, int num2);
+    }
+
+
 }
--- src/com/mainacad/model/ConnectionInfo.java
+++ src/com/mainacad/model/ConnectionInfo.java
@@ -1,44 +1,17 @@
 package com.mainacad.model;
 
+import lombok.*;
+
 import java.io.Serializable;
 
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class ConnectionInfo implements Serializable {
     private String ip;
     private Integer sessionId;
     private Long connectionTime;
-
-    public ConnectionInfo(String ip, Integer sessionId, Long connectionTime) {
-        this.ip = ip;
-        this.sessionId = sessionId;
-        this.connectionTime = connectionTime;
-    }
-
-    public ConnectionInfo() {
-    }
-
-    public String getIp() {
-        return ip;
-    }
-
-    public void setIp(String ip) {
-        this.ip = ip;
-    }
-
-    public Integer getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(Integer sessionId) {
-        this.sessionId = sessionId;
-    }
-
-    public Long getConnectionTime() {
-        return connectionTime;
-    }
-
-    public void setConnectionTime(Long connectionTime) {
-        this.connectionTime = connectionTime;
-    }
 
     @Override
     public String toString() {
